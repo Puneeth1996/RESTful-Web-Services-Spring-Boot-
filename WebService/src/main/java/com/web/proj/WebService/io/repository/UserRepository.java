57ddafdd0072947d1906f3854ec4a311c08620a4
Puneeth1996 <puneeth1996p@gmail.com>
2024-07-03 21:46:41 +0530

    kept the webservices repository in working condition by fixing the issues

--- WebService/src/main/java/com/web/proj/WebService/io/repository/UserRepository.java
+++ WebService/src/main/java/com/web/proj/WebService/io/repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
     UserEntity findByUserId(String userId);
--- WebService/src/main/java/com/web/proj/WebService/service/impl/UserServiceImpl.java
+++ WebService/src/main/java/com/web/proj/WebService/service/impl/UserServiceImpl.java
@@ -10,16 +10,13 @@ import com.web.proj.WebService.shared.Utils;
 import com.web.proj.WebService.ui.modal.request.UserLoginRequestModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
